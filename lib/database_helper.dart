96312776d5a7676ebf7adc4b2cc9e1c933c7f4cf
artemreva79 <artemreva79@gmail.com>
2025-05-18 13:05:14 +0000

    add sqlite

---
+++ lib/database_helper.dart
@@ -0,0 +1,45 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._();
+  static Database? _database;
+
+  DatabaseHelper._();
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'translations.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE Translations(
+        vocabulary TEXT NOT NULL,
+        original_word TEXT NOT NULL,
+        origin_lang TEXT NOT NULL,
+        transl_word TEXT NOT NULL,
+        transl_code TEXT NOT NULL,
+        learned INTEGER
+      )
+    ''');
+  }
+}
